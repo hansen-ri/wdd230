dde1ea1d023d8b937784278553a38bffaccbcf63
Riley Hansen <72674034+hansen-ri@users.noreply.github.com>
2022-01-15 23:38:16 -0700

    Delete lesson2 directory

--- lesson2/js2/script2.js
+++
@@ -1,6 +0,0 @@
-let currentdate = document.lastModified;
-let d = new Date();
-let currentyear = d.getFullYear();
-document.querySelector(
-  "#pagecredits"
-).innerHTML = `&copy; ${currentyear} | Riley Grant Hansen | Last Updated: ${currentdate}`;
