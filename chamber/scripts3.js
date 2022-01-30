e434090bbb76574e012ca34deccdaba032392108
hansen-ri <gridironhero@gmail.com>
2022-01-29 18:21:19 -0700

    Responsive Menu

---
+++ chamber/scripts3.js
@@ -0,0 +1,30 @@
+let currentdate = document.lastModified;
+document.getElementById("update").textContent = `Last Updated: ${currentdate}`;
+
+let d = new Date();
+let currentyear = d.getFullYear();
+document.querySelector(
+  "#pagecredits"
+).innerHTML = `&copy; ${currentyear} goodyearcoc.com .:|:. Riley G. Hansen .:|:. BYU-Idaho .:|:. WDD 130 Project .:|:.`;
+
+const datefield = document.querySelector("date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
+datefield.innerHTML = `<strong>${fulldate}</strong>`;
+
+const hambutton = document.querySelector(".ham");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener(
+  "click",
+  () => {
+    mainnav.classList.toggle("responsive");
+  },
+  false
+);
+
+window.onresize = () => {
+  if (window.innerWidth > 647) mainnav.classList.remove("responsive");
+};
--- lesson3/scripts3.js
+++
@@ -1,6 +0,0 @@
-let currentdate = document.lastModified;
-document.getElementById("update").textContent = `Last Updated: ${currentdate}`;
-
-let d = new Date();
-let currentyear = d.getFullYear();
-document.querySelector("#pagecredits").innerHTML = `&copy; ${currentyear} goodyearcoc.com .:|:. Riley G. Hansen .:|:. BYU-Idaho`;
