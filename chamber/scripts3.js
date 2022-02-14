3460bb16f24b9bf0cb6b5465fc5660c8822db5e6
hansen-ri <gridironhero@gmail.com>
2022-02-14 11:35:57 -0700

    Major layout

--- chamber/scripts3.js
+++ chamber/scripts3.js
@@ -7,12 +7,14 @@ document.querySelector(
   "#pagecredits"
 ).innerHTML = `&copy; ${currentyear} goodyearcoc.com .:|:. Riley G. Hansen .:|:. BYU-Idaho .:|:. WDD 130 Project .:|:.`;
 
-const datefield = document.querySelector("date");
+const datefield = document.querySelector("#date");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
   now
 );
-datefield.innerHTML = `<strong>${fulldate}</strong>`;
+
+// if (fulldate.includes("Monday") || fulldate.includes("Tuesday"));
+  datefield.innerHTML = `<strong>${fulldate}</strong>`;
 
 const hambutton = document.querySelector(".ham");
 const mainnav = document.querySelector(".navigation");
