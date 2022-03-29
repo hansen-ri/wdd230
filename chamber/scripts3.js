3ab713ab6511c0cab65cfe34b772613dff4623b4
hansen-ri <gridironhero@gmail.com>
2022-03-29 10:21:05 -0600

    Chamber Update

--- chamber/scripts3.js
+++ chamber/scripts3.js
@@ -35,3 +35,21 @@ hambutton.addEventListener(
 window.onresize = () => {
   if (window.innerWidth > 647) mainnav.classList.remove("responsive");
 };
+
+// Weather API 
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5296266&appid=73b8add6eff6e1a81677303097bb30a6";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.querySelector('#current-temp').textContent = ((jsObject.main.
+    temp - 273.15) * 1.8 + 32).toFixed(1);
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;    
+  });
+  // Weather API
