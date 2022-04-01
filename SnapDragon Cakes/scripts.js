78b44fafa70fe3520ecb590e17517e889a13c8aa
hansen-ri <gridironhero@gmail.com>
2022-04-01 16:48:19 -0600

    Home & FAQs

--- SnapDragon Cakes/scripts.js
+++ SnapDragon Cakes/scripts.js
@@ -23,3 +23,21 @@ window.onresize = () => {
   if (window.innerWidth > 450) subnav.classList.remove("display-menu");
 };
 // /\ /\ /\
+
+// SlideShow logic
+//  \/ \/ \/
+// var myIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1}    
+//   x[myIndex-1].style.display = "block";  
+//   setTimeout(carousel, 2500);    
+// }
+// /\ /\ /\
